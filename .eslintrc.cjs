296885db6eb11fdc2efa102af008933847f17a09
matheuscvp <matheuscvp12@gmail.com>
2022-08-08 08:45:51 -0300

    chore: acrescenta configurações de eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,8 @@ module.exports = {
     'airbnb',
     'prettier',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
