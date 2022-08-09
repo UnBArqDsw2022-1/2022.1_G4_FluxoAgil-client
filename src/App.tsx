9fd3191413234de8d9c77b928af8d96afcf67ef4
Irwin Schmitt <irwin.schmitt@quintoandar.com.br>
2022-08-09 14:39:56 -0300

    feat: cria header
    Co-authored-by: Matheus Fonseca Sousa <matheus.fsousa11@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { AppBar, Box, Toolbar } from '@mui/material'
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material'
 import Button from './components/Button'
 
 function App() {
   return (
     <AppBar>
-      <Toolbar>
-        <Box width="100%" display="flex">
-          <div>Fluxo Ágil</div>
-        </Box>
+      <Container>
+        <Toolbar>
+          <Box width="100%" display="flex">
+            <Typography>
+              <Box fontWeight="bold">Fluxo Ágil</Box>
+            </Typography>
+          </Box>
 
-        <Box display="flex">
-          <Button>Docs</Button>
+          <Box display="flex">
+            <Button
+              href="https://github.com/UnBArqDsw2022-1/2022.1_G4_FluxoAgil"
+              target="_blank"
+            >
+              Docs
+            </Button>
 
-          <Button>Sobre</Button>
-        </Box>
-      </Toolbar>
+            <Button disabled>Sobre</Button>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
