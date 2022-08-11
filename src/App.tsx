667ce60d692cd2948a1fb956e721a6e181991f86
Irwin Schmitt <irwin.schmitt@quintoandar.com.br>
2022-08-11 16:55:56 -0300

    feat: cria header (#21)
    * feat: cria header
    
    Co-authored-by: Matheus Fonseca Sousa <matheus.fsousa11@gmail.com>
    
    * feat: cria header
    
    Co-authored-by: Matheus Fonseca Sousa <matheus.fsousa11@gmail.com>
    
    * feat: encapsula header
    
    Co-authored-by: Matheus Fonseca Sousa <matheus.fsousa11@gmail.com>
    
    * feat: remove encapsulamento do botão
    
    Co-authored-by: Matheus Fonseca Sousa <matheus.fsousa11@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,7 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button'
-import reactLogo from './assets/react.svg'
-import fluxoAgilLogo from './assets/logo.png'
-import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a
-          href="https://github.com/UnBArqDsw2022-1/2022.1_G4_FluxoAgil-web"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={fluxoAgilLogo}
-            className="logo react"
-            alt="Logo fluxo agil"
-          />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Fluxo Ágil</h1>
-      <h3>
-        Em breve você terá a experiência mais agradável de recomendação de
-        disciplinas da UnB
-      </h3>
-      <div className="card">
-        <p>Enquanto isso, brinque com o botão:</p>
-        <Button variant="contained" onClick={() => setCount(count + 1)}>
-          Este botão clicado {count} vezes
-        </Button>
-
-        <p>
-          Saiba mais clicando{' '}
-          <a href="https://unbarqdsw2022-1.github.io/2022.1_G4_FluxoAgil/">
-            aqui
-          </a>
-          .
-        </p>
-      </div>
-    </div>
-  )
+  return <Header />
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+
+export default function Header() {
+  return (
+    <AppBar elevation={0}>
+      <Container>
+        <Toolbar>
+          <Box width="100%" display="flex">
+            <Typography>
+              <Box fontWeight="bold">Fluxo Ágil</Box>
+            </Typography>
+          </Box>
+
+          <Box display="flex">
+            <Button
+              color="secondary"
+              href="https://github.com/UnBArqDsw2022-1/2022.1_G4_FluxoAgil"
+              target="_blank"
+            >
+              Docs
+            </Button>
+
+            <Button color="secondary" disabled>
+              Sobre
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
