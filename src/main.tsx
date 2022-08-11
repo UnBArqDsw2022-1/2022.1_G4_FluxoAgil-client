2951fae9451ab72eddc4c3e42b097b76eec58a38
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-08-10 22:47:10 -0300

    chore: adiciona nova organização

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from '@mui/material/styles'
 import App from './App'
-import './index.css'
-import theme from './utilities/theme'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 )
---
+++ src/pages/PageTest/index.tsx
@@ -0,0 +1,10 @@
+import { Overview } from '../../components/Overview'
+
+export const PageTest = () => {
+  return (
+    <div>
+      <h1>componente teste </h1>
+      <Overview />
+    </div>
+  )
+}
