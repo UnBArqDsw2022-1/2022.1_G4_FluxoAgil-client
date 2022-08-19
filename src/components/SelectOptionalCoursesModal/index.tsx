9be3748cdefd596a2a2bda55924c72dc9efb3218
Yudi Yamane <yudiyamane@gmail.com>
2022-08-19 08:26:49 -0300

    refactor: muda opções para cursos de verdade

--- src/components/SelectOptionalCoursesModal/index.tsx
+++ src/components/SelectOptionalCoursesModal/index.tsx
@@ -18,79 +18,12 @@ interface Props {
   setSelectedOptionalCourses: (courses: OptionalCourse[]) => void
 }
 
-const top100Films: OptionalCourse[] = [
-  { label: 'The Shawshank Redemption', workloadInHours: 1994 },
-  { label: 'The Godfather', workloadInHours: 1972 },
-  { label: 'The Godfather: Part II', workloadInHours: 1974 },
-  { label: 'The Dark Knight', workloadInHours: 2008 },
-  { label: '12 Angry Men', workloadInHours: 1957 },
-  { label: "Schindler's List", workloadInHours: 1993 },
-  { label: 'Pulp Fiction', workloadInHours: 1994 },
-  {
-    label: 'The Lord of the Rings: The Return of the King',
-    workloadInHours: 2003,
-  },
-  { label: 'The Good, the Bad and the Ugly', workloadInHours: 1966 },
-  { label: 'Fight Club', workloadInHours: 1999 },
-  {
-    label: 'The Lord of the Rings: The Fellowship of the Ring',
-    workloadInHours: 2001,
-  },
-  {
-    label: 'Star Wars: Episode V - The Empire Strikes Back',
-    workloadInHours: 1980,
-  },
-  { label: 'Forrest Gump', workloadInHours: 1994 },
-  { label: 'Inception', workloadInHours: 2010 },
-  {
-    label: 'The Lord of the Rings: The Two Towers',
-    workloadInHours: 2002,
-  },
-  { label: "One Flew Over the Cuckoo's Nest", workloadInHours: 1975 },
-  { label: 'Goodfellas', workloadInHours: 1990 },
-  { label: 'The Matrix', workloadInHours: 1999 },
-  { label: 'Seven Samurai', workloadInHours: 1954 },
-  {
-    label: 'Star Wars: Episode IV - A New Hope',
-    workloadInHours: 1977,
-  },
-  { label: 'City of God', workloadInHours: 2002 },
-  { label: 'Se7en', workloadInHours: 1995 },
-  { label: 'The Silence of the Lambs', workloadInHours: 1991 },
-  { label: "It's a Wonderful Life", workloadInHours: 1946 },
-  { label: 'Life Is Beautiful', workloadInHours: 1997 },
-  { label: 'The Usual Suspects', workloadInHours: 1995 },
-  { label: 'Léon: The Professional', workloadInHours: 1994 },
-  { label: 'Spirited Away', workloadInHours: 2001 },
-  { label: 'Saving Private Ryan', workloadInHours: 1998 },
-  { label: 'Once Upon a Time in the West', workloadInHours: 1968 },
-  { label: 'American History X', workloadInHours: 1998 },
-  { label: 'Interstellar', workloadInHours: 2014 },
-  { label: 'Casablanca', workloadInHours: 1942 },
-  { label: 'City Lights', workloadInHours: 1931 },
-  { label: 'Psycho', workloadInHours: 1960 },
-  { label: 'The Green Mile', workloadInHours: 1999 },
-  { label: 'The Intouchables', workloadInHours: 2011 },
-  { label: 'Modern Times', workloadInHours: 1936 },
-  { label: 'Raiders of the Lost Ark', workloadInHours: 1981 },
-  { label: 'Rear Window', workloadInHours: 1954 },
-  { label: 'The Pianist', workloadInHours: 2002 },
-  { label: 'The Departed', workloadInHours: 2006 },
-  { label: 'Terminator 2: Judgment Day', workloadInHours: 1991 },
-  { label: 'Back to the Future', workloadInHours: 1985 },
-  { label: 'Whiplash', workloadInHours: 2014 },
-  { label: 'Gladiator', workloadInHours: 2000 },
-  { label: 'Memento', workloadInHours: 2000 },
-  { label: 'The Prestige', workloadInHours: 2006 },
-  { label: 'The Lion King', workloadInHours: 1994 },
-  { label: 'Apocalypse Now', workloadInHours: 1979 },
-  { label: 'Alien', workloadInHours: 1979 },
-  { label: 'Sunset Boulevard', workloadInHours: 1950 },
-  {
-    label:
-      'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    workloadInHours: 1964,
-  },
+const courses: OptionalCourse[] = [
+  { label: 'Cálculo numérico', workloadInHours: 60 },
+  { label: 'Desenho mecânico', workloadInHours: 90 },
+  { label: 'Algoritmo e programação de computadores', workloadInHours: 90 },
+  { label: 'Introdução à engenharia', workloadInHours: 30 },
+  { label: 'Humanidades e cidadania', workloadInHours: 60 },
 ]
 
 export default function SelectOptionalCoursesModal({
@@ -106,7 +39,7 @@ export default function SelectOptionalCoursesModal({
   useEffect(() => {
     // response = await axios.get('/optional-courses?course_id=xxxx&curriculae_id=${curriculaeId}')
     // setOptionalCourses(response.data)
-    setOptionalCourses(top100Films)
+    setOptionalCourses(courses)
   }, [])
 
   return (
@@ -134,28 +67,28 @@ export default function SelectOptionalCoursesModal({
         </Typography>
 
         <Autocomplete
+          multiple
+          options={optionalCourses}
           value={selectedOptionalCourses}
           onChange={(event, newSelectedCourses) =>
             setSelectedOptionalCourses(newSelectedCourses)
           }
           inputValue={inputValue}
           onInputChange={(event, newValue) => setInputValue(newValue)}
-          multiple
-          options={optionalCourses}
           getOptionLabel={option =>
-            `${option.label} (${option.workloadInHours})`
+            `${option.label} (${option.workloadInHours}h)`
           }
           renderInput={params => (
-            <TextField variant="standard" {...params} label="Movie" />
+            <TextField
+              variant="standard"
+              label="Cursos optativos"
+              {...params}
+            />
           )}
         />
 
         <Grid container justifyContent="space-between" mt="64px">
-          <Button
-            onClick={() => handleClose()}
-            variant="outlined"
-            color="secondary"
-          >
+          <Button onClick={handleClose} variant="outlined" color="secondary">
             Cancelar
           </Button>
           <Button variant="contained" color="secondary">
