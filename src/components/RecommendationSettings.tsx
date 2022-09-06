0aa97e308f940c6d85abfe8ccd6b117111d871fc
Irwin Schmitt <irwinschmitt@gmail.com>
2022-09-05 22:16:55 -0300

    feat: adiciona carga horária

--- src/components/RecommendationSettings.tsx
+++ src/components/RecommendationSettings.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   Alert,
   Box,
@@ -10,13 +10,58 @@ import {
 } from '@mui/material'
 import { InfoOutlined } from '@mui/icons-material'
 import SelectOptionalCoursesModal from '@/components/SelectOptionalCoursesModal'
+import { selectAcademicHistory } from '@/store/recommendation'
+import { useSelector } from 'react-redux'
+
+const getPercentage = (partial: number, total: number) => {
+  const percentage = (partial / total) * 100
+  return Math.round(percentage * 100) / 100
+}
 
 export default function RecommendationSettings() {
+  const academicHistory = useSelector(selectAcademicHistory)
+  const [totalIntegrated, setTotalIntegrated] = useState(0)
+  const [mandatoryIntegrated, setMandatoryIntegrated] = useState(0)
+  const [optionalIntegrated, setOptionalIntegrated] = useState(0)
+  const [optionalPendingInHours, setOptionalPendingInHours] = useState(0)
+  const [optionalPendingInCredits, setOptionalPendingInCredits] = useState(0)
+  const [supplementaryIntegrated, setSupplementaryIntegrated] = useState(0)
   const [maxCredits, setMaxCredits] = useState(24)
   const [
     isSelectOptionalCoursesModalOpen,
     setIsSelectOptionalCoursesModalOpen,
   ] = useState(false)
+
+  useEffect(() => {
+    if (!academicHistory) {
+      return
+    }
+
+    const {
+      workload: { total, mandatory, optional, supplementary },
+    } = academicHistory
+
+    const totalPercentage = getPercentage(total.integrated, total.required)
+    const mandatoryPercentage = getPercentage(
+      mandatory.integrated,
+      mandatory.required
+    )
+    const optionalPercentage = getPercentage(
+      optional.integrated,
+      optional.required
+    )
+    const supplementaryPercentage = getPercentage(
+      supplementary.integrated,
+      supplementary.required
+    )
+
+    setTotalIntegrated(totalPercentage)
+    setMandatoryIntegrated(mandatoryPercentage)
+    setOptionalIntegrated(optionalPercentage)
+    setOptionalPendingInCredits(optional.pending / 15)
+    setOptionalPendingInHours(optional.pending)
+    setSupplementaryIntegrated(supplementaryPercentage)
+  }, [academicHistory])
 
   const handleMaxCreditsChange = (
     event: Event,
@@ -29,8 +74,14 @@ export default function RecommendationSettings() {
 
   return (
     <Box border="2px solid #0CABA8" borderRadius="5px">
-      <Grid container p={5} minHeight="300px">
-        <Grid item xs={12} sm={6}>
+      <Grid
+        container
+        p={5}
+        minHeight="300px"
+        spacing={4}
+        justifyContent="center"
+      >
+        <Grid item xs={12} sm={8} md={6}>
           <Box
             display="flex"
             flexDirection="column"
@@ -42,19 +93,33 @@ export default function RecommendationSettings() {
               Visão geral
             </Typography>
 
-            <Typography>Total integralizado: 11%</Typography>
-            <Typography>Obrigatórias integralizadas: </Typography>
-            <Typography>Optativas integralizadas: </Typography>
+            <Typography>
+              Total integralizado: <strong>{totalIntegrated}%</strong>
+            </Typography>
+            <Typography>
+              Obrigatórios integralizados:{' '}
+              <strong>{mandatoryIntegrated}%</strong>
+            </Typography>
+            <Typography>
+              Optativos integralizados: <strong>{optionalIntegrated}%</strong>
+            </Typography>
+
+            {!Number.isNaN(supplementaryIntegrated) && (
+              <Typography>
+                Complementares integralizados:{' '}
+                <strong>{supplementaryIntegrated}%</strong>
+              </Typography>
+            )}
           </Box>
         </Grid>
 
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={8} md={6}>
           <Box display="flex" flexDirection="column">
             <Box display="flex" alignItems="center" pb={1}>
               <Typography pr={1}>
                 Até <strong>{maxCredits} créditos</strong> por semestre
               </Typography>
-              <Tooltip title="A quantidade máxima de créditos que será recomendada em disciplinas em cada semestre.">
+              <Tooltip title="Quantidade máxima de créditos em disciplinas que será recomendada em cada semestre.">
                 <InfoOutlined fontSize="small" />
               </Tooltip>
             </Box>
@@ -71,7 +136,8 @@ export default function RecommendationSettings() {
           <Box mt={3}>
             <Alert severity="info">
               <Typography>
-                Ainda faltam XX em optativas para você se formar.
+                Ainda faltam {optionalPendingInCredits} créditos (
+                {optionalPendingInHours}h) em optativas para você se formar.
               </Typography>
             </Alert>
 
